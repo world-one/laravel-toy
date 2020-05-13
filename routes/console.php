746b683931dcc0cebe2d74f1ba575a52c372ac82
world-one <worldone85@gmail.com>
2020-05-13 14:33:12 +0900

    first && console test

---
+++ routes/console.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
+|
+*/
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->describe('Display an inspiring quote');
+
+
+Artisan::command('gogogo', function () {
+    $this->commonet( 'php artisan list');
+})->describe('how');
+
+Artisan::command('desc:console {desc}', function ($desc) {
+    echo $desc."\n";
+})->describe('text print');
